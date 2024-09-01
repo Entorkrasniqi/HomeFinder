d38367930c011fd20cee0f8c9e8f699a7836ae66
Azzni96 <nihadazzam96@gmail.com>
2024-09-01 18:25:26 +0300

    korjaus

--- FRONTEND/script.js
+++ FRONTEND/script.js
@@ -1,5 +1,5 @@
 const bar = document.getElementById('bar');
-const close = document.getElementById('bar');
+const close = document.getElementById('close');
 const nav = document.getElementById('navbar');
 
 if (bar) {
